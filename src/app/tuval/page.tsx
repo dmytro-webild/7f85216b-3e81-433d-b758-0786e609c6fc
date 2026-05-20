6b9e9086bb3d2faca8b9533da8b4fb59350997f0
bender <kudin.dima@gmail.com>
2026-05-20 10:51:10 +0000

    Switch to version 11: modified src/app/tuval/page.tsx

--- src/app/tuval/page.tsx
+++ src/app/tuval/page.tsx
@@ -38,6 +38,7 @@ export default function TuvalPage() {
             <NavbarStyleFullscreen
               navItems={navItems}
               brandName="Deniz Çerçeve"
+              button={{ text: "Teklif Al", href: "#contact" }}
             />
         </div>
 
